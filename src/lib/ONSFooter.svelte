3864fac71aaa811ec076530d7b5ec05e2cc4b0de
faeez safedien <faeez safedien>
2023-04-06 14:48:38 +0700

    MVP

---
+++ src/lib/ONSFooter.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { themes } from '../config.js';
+	import { getContext } from 'svelte';
+
+	export let theme = getContext('theme');
+</script>
+
+<footer style="--text: {themes[theme]['text']}; --muted: {themes[theme]['muted']}; --pale: {themes[theme]['pale']};">
+	<div class="col-wide" data-analytics="footer">
+		
+		<div class="license">
+			All content is available under the
+			<a
+				href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+				class="external-link"
+				target="_blank"
+				rel="noopener">Open Government Licence v3.0</a>, 
+			except where otherwise stated
+		</div>
+	</div>
+</footer>
+
+<style>
+	footer {
+		color: var(--text);
+		background-color: var(--pale);
+		padding-bottom: 72px;
+		padding-top: 36px;
+		font-size: 18px;
+	}
+	footer a {
+		-webkit-box-sizing: border-box;
+		box-sizing: border-box;
+		color: var(--muted);
+		text-decoration: underline;
+		display: inline-block;
+	}
+	footer a:hover {
+		color: var(--text);
+		text-decoration: none;
+	}
+	footer a:active {
+		outline: 3px solid transparent;
+		background-color: #fd0;
+		-webkit-box-shadow: 0 -2px #fd0, 0 4px #222;
+		box-shadow: 0 -2px #fd0, 0 4px #222;
+		color: #222;
+		text-decoration: none;
+	}
+	.license {
+		vertical-align: top;
+	}
+</style>
---
+++ src/lib/ONSHeader.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { themes } from '../config.js';
+	import { getContext } from 'svelte';
+
+	export let theme = getContext('theme');
+</script>
+
+<footer style="color: {themes[theme]['text']}; background-color: {themes[theme]['pale']};">
+	<div class="col-wide" data-analytics="footer">
+		<a href="https://www.ons.gov.uk/">
+			{#if theme == 'dark'}
+			<img
+				class="logo-img"
+				src="./img/ons-logo-neg-en.svg"
+				alt="Office for National Statistics" />
+			{:else}
+			<img
+				class="logo-img"
+				src="./img/ons-logo-black-en.svg"
+				alt="Office for National Statistics" />
+			{/if}
+		</a>
+		<ul>
+			<li><a href="https://www.ons.gov.uk/aboutus/contactus" class="link" style="color: {themes[theme]['text']}">Contact us</a></li>
+			<li><a href="https://www.ons.gov.uk/help/privacynotice" class="link" style="color: {themes[theme]['text']}">Cookies and privacy</a></li>
+		</ul>
+	</div>
+</footer>
+
+<style>
+	footer {
+		padding-bottom: 72px;
+		padding-top: 36px;
+		font-size: 18px;
+	}
+	footer a.link {
+		-webkit-box-sizing: border-box;
+		box-sizing: border-box;
+		text-decoration: underline;
+		display: inline-block;
+	}
+	footer a.link:hover {
+		text-decoration: none;
+	}
+	footer a.link:active {
+		outline: 3px solid transparent;
+		background-color: #fd0;
+		-webkit-box-shadow: 0 -2px #fd0, 0 4px #222;
+		box-shadow: 0 -2px #fd0, 0 4px #222;
+		color: #222;
+		text-decoration: none;
+	}
+	ul {
+		padding: 0;
+		margin: 0;
+	}
+	li {
+		margin-right: 18px;
+		display: inline-block;
+	}
+	.logo-img {
+		margin-bottom: 27px;
+	}
+	a img:hover {
+		cursor: pointer;
+	}
+</style>
